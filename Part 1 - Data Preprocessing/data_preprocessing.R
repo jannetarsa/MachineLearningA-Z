9932a69f0000c42ead59157564e78db8d4e1675c
jannetarsa <janne.tarsa@gmail.com>
2017-11-26 12:32:17 +0200

    Part 1 Data preprosessing template

---
+++ Part 1 - Data Preprocessing/data_preprocessing.R
@@ -0,0 +1,28 @@
+# Data preprocessing
+dataset = read.csv('Data.csv')
+
+# Handling missing data
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE )),
+                     dataset$Age)
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                      ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE )),
+                      dataset$Salary)
+
+# Encoding categorial variables
+dataset$Country = factor(dataset$Country,
+                         levels = c('France', 'Spain', 'Germany'),
+                         labels = c(1, 2, 3))
+dataset$Purchased = factor(dataset$Purchased,
+                           levels = c('No', 'Yes'),
+                           labels = c(0, 1))
+# Splitting the data into the training and test data sets
+#install.packages('caTools')
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature scaling
+training_set[, 2:3] = scale(training_set[, 2:3])
+test_set[, 2:3] = scale(test_set[, 2:3])
---
+++ Part 1 - Data Preprocessing/data_preprocessing.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+#Importing libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+"""
+Created on Mon Nov 20 22:03:44 2017
+
+@author: janne_000
+"""
+dataset = pd.read_csv('Data.csv')
+# take all values except last column
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
+
+# Handling missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values='NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+# Splitting the data into the training and test data sets
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
