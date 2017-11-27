f3eee6cfd4caab383cbe96505b852a52eee95ed2
jannetarsa <janne.tarsa@gmail.com>
2017-11-27 22:22:50 +0200

    Part 2 Simple Linear Regression Step 3

--- Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
@@ -27,6 +27,10 @@ sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)"""
 
+# Fitting simple linear regression to test set
 from sklearn.linear_model import LinearRegression
-reressor = LinearRegression()
+regressor = LinearRegression()
 regressor.fit(X_train, y_train)
+
+# Predicting the test set results
+y_pred = regressor.predict(X_test)
